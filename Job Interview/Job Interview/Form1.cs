cae4bc00f0d0e71bd7bdc6044b3fdd67a7de10af
Robert Davis <me@robertdavis.co.uk>
2016-03-01 15:27:27 +0000

    Further UI Implementation (Trying again)

--- Job Interview/Job Interview/Form1.cs
+++ Job Interview/Job Interview/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Job_Interview
 {
-    public partial class Form1 : Form
+    public partial class OnlineInterviewFormBackground : Form
     {
-        public Form1()
+        public OnlineInterviewFormBackground()
         {
             InitializeComponent();
         }
