f7cfc0eb66f92dd912282d70e02de950f9313037
jamesbruce12 <jamesbruce10@hotmail.co.uk>
2016-05-02 16:11:46 +0100

    tidied up the AI files
    put all the old files in its own folder, and current files has its own
    too

---
+++ Prolog AI/Current AI/AIGroupP.pl
@@ -0,0 +1,51 @@
+% kathy(Stimuli, Response). to get it to work
+kathy(Stimuli, Response) :-
+    open('Input.txt', read, Str),
+    read(Str,Stimuli),
+    close(Str),
+    write(Stimuli),
+    template(InternalStimuli, InternalResponse),
+    match(InternalStimuli, Stimuli),
+    match(InternalResponse, Response),
+    open('Output.txt',write,Stream),
+    write(Stream,Response),  nl(Stream),
+    close(Stream),
+    !.
+
+/** Need some massive improvements on here **/
+/** Might rewrite all the templates, so that each line is more effienct, so that it only requires 1 line for each section rather than multiple ones **/
+/** hello! **/
+template([s([hello])], [s([hello, how, are, you,?])]).
+template([s([hey])], [s([hello, how, are, you,?])]).
+template([s([hi])], [s([hello, how, are, you,?])]).
+
+/** saying im fine! **/
+template([s([im, fine, how, are, you, ?])], [s([im, fine, too, thanks, for, asking, are, you, ready, to, start, the, interview, ?])]).
+template([s([im, fine, you, ?])], [s([im, fine, too, thanks, for, asking, are, you, ready, to, start, the, interview, ?])]).
+template([s([im, fine])], [s([ok, are, you, ready, to, start, the, interview, ?])]).
+
+/** initiating questions! **/
+template([s([yes])], [s([what, role, do, you, usually, take, when, working, in, a, team, ?])]).
+template([s([ok])], [s([what, is, your, usual, role, in, a, team, ?])]).
+template([s([lets, begin])], [s([what, is, your, usual, role, in, a, team, ?])]).
+
+/** replying to user responces! **/
+/** currently needs improvement! **/
+template([s([i, am]), s(X)], [s([why,are,you]),s(X),w('?')]).
+template([s(X), s([because])], [s([thank, you, for, telling, me, more, about]), s(X)]).
+template([w(i),s(X),w(you)], [s([why,do,you]),s(X),w(me),w('?')]).
+
+
+
+
+
+match([],[]).
+match([Item|Items],[Word|Words]) :-
+    match(Item, Items, Word, Words).
+
+match(w(Word), Items, Word, Words) :-
+    match(Items, Words).
+
+match(s([Word|Seg]), Items, Word, Words0) :-
+    append(Seg, Words1, Words0),
+    match(Items, Words1).
---
+++ Prolog AI/previous files/Prolog/README.txt
@@ -0,0 +1,7 @@
+	Input.txt
+
+Whatever is on this file is what Stimuli becomes, '[]' and ',' need to be added by the C# program in order for this to work.
+
+	Output.txt
+
+Response gets writen to the text file, needs to  have the '[]' and ',' removed by the C# program, just so that the response is simpler to read.
